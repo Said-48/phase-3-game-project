13c0481d6a67ed3598b08085f86bcdc42c624f36
saidabdulhakim48@gmail.com <saidabdulhakim48@gmail.com>
2025-09-01 20:34:56 +0300

    defined classes player and gamesession models

--- dinosaur/db.py
+++ dinosaur/db.py
@@ -5,3 +5,15 @@ base = declarative_base()
 engine = create_engine("sqlite:///dino.db")
 session = sessionmaker(bind=engine)
 
+class Player(base):
+    __tablename__ = "players"
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    score = Column(Integer)
+    sessions = relationship("GameSession", backref="player")
+
+class GameSession(base):
+    __tablename__ = "game_session"
+    id = Column(Integer, primary_key=True)
+    player_id = Column(Integer, ForeignKey("players.id"))
+    score = Column(Integer)
